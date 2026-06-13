70d250a636ee0c6fe54761888439db69c560acf4
Kateryna Kravchunovska <kravchunovska.katerina@gmail.com>
2026-06-13 13:07:53 +0300

    change db

--- db_flask.py
+++ db_flask.py
@@ -1,16 +1,18 @@
+import os
 import mysql.connector
 from mysql.connector import Error
 
 def get_db_connection():
+    config = {
+        'host': os.environ.get('MYSQLHOST', 'localhost'),
+        'port': os.environ.get('MYSQLPORT', '3306'),
+        'user': os.environ.get('MYSQLUSER', 'root'),
+        'password': os.environ.get('MYSQLPASSWORD', ''),
+        'database': os.environ.get('MYSQLDATABASE', 'admission_system'),
+        'autocommit': True
+    }
     try:
-        connection = mysql.connector.connect(
-            host="localhost",
-            port="3306",
-            database="admission_system",
-            user="root",
-            password="152845",
-            autocommit=True
-        )
+        connection = mysql.connector.connect(**config)
         return connection
     except Error as e:
         print(f"[DB ERROR] {e}")
